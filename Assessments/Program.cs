79f9b75d1a4cbba3aecf786273da5fa1f12f232f
Karol Morgan <karolyne.morgan@outlook.com>
2024-06-13 13:53:18 -0400

    Add project files.

---
+++ Assessments/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assessments;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //FIZZBUZZ
+        for (int i = 1; i < 31; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.Write(" FIZZBUZZ ");
+            } else if (i % 3 == 0)
+            {
+                Console.Write(" FIZZ ");
+            } else if (i % 5 == 0)
+            {
+                Console.Write(" BUZZ ");
+            } else
+            {
+                Console.Write($" {i} ");
+            }
+        }
+
+        
+    }
+}
